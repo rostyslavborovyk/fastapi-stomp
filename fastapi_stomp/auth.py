187e555baeb83a7ad1833700302dbe9e32092ede
rostyslav <rostmoguchiy@gmail.com>
2023-03-01 23:07:49 +0200

    Updated auth

--- fastapi_stomp/auth.py
+++ fastapi_stomp/auth.py
@@ -2,13 +2,17 @@
 Authentication providers.
 """
 import abc
+import typing as t
+
+TUser = t.TypeVar("TUser")
 
 
-class AsyncAuthenticator(abc.ABC):
+class AsyncAuthenticator(abc.ABC, t.Generic[TUser]):
     """ Abstract base class for authenticators. """
+    user: TUser
 
     @abc.abstractmethod
-    async def authenticate_from_token(self, token: str):
+    async def authenticate_from_token(self, token: str) -> bool:
         """
         Authenticate with the Alpha Zulu token
         """
